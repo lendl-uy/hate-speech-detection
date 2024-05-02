7e399b2058a32cbbdcc495f2abfae59130e6fec0
Ryan Roi Cayas <rrcayas@gmail.com>
2024-05-03 01:13:50 +0800

    updated cleaning pipeline

--- dataset/dataset_constants.py
+++ dataset/dataset_constants.py
@@ -1,3 +1,7 @@
 TRAIN_DATASET_PATH = "train.csv"
 VALIDATION_DATASET_PATH = "valid.csv"
 TEST_DATASET_PATH = "test.csv"
+CLEANED_DATASET_PATH = "cleaned_dataset.pkl"
+STOPWORDS_ENGLISH_PATH = "stopwords/english"
+STOPWORDS_TAGALOG_PATH = "stopwords/tagalog"
+split_sizes=[10000, 4232, 4232]
